694c86e821b17a1e83bdccf062d09d5c40170bd1
ggsimo35-cloud <ggsimo35@gmail.com>
2026-08-04 19:21:31 +0100

    Update bot token for Telegram bot

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from aiogram.filters import Command
 from aiogram.types import LabeledPrice, PreCheckoutQuery
 
 
-BOT_TOKEN = "8882282653:AAGieiKuEadfsfE88baF4dYZrfBUSyjqiCQ"
+BOT_TOKEN = "8882282653:AAGTmKMckUAyFl_YYUv03D-FZ6dlBXZG3eo"
 
 CHANNEL_ID = -1004424805545
 
